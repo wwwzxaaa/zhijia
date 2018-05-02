d6800fb32efcab21846157a0c3a6fe13f619cb8f
Mxtzz <mazw2016@hotmail.com>
2018-05-02 15:41:25 +0800

    更新时间戳，更新广场页显示效果。实现发帖功能

--- ionic/src/pages/publish/publish.ts
+++ ionic/src/pages/publish/publish.ts
@@ -25,17 +25,21 @@ export class PublishPage {
   ) {
     // 接收发布页面数据
     events.subscribe('user:created',(textarea,time) => {
-      console.log(textarea,time);
-      this.publish.unshift({icon:'assets/publish/zhijia.png',name:'知家官方团队',pic:'',article:textarea});
+      console.log(textarea,this.timestampToTime(time));
+      var theTime = this.timestampToTime(time);
+      this.publish.unshift({
+        icon:'assets/publish/zhijia.png',name:'知家官方团队',
+        time:theTime,pic:'',article:textarea
+      });
     })
   }
 
   publish=[
     {
-      icon:'assets/publish/mht.jpg',name:'Pony Ma',
+      icon:'assets/publish/mht.jpg',name:'Pony Ma',time:'2018年5月2日',
       pic:'assets/publish/main.jpg',article:'来充钱啊不充钱你怎么变得更强！！！'
     },{
-      icon:'assets/publish/my.jpg',name:'Jack Ma',
+      icon:'assets/publish/my.jpg',name:'Jack Ma',time:'2018年5月2日',
       pic:'assets/publish/alibaba.jpg',article:'我对钱没有兴趣！我最后悔的就是创立了阿里巴巴！！'
     }
   ]
@@ -69,6 +73,16 @@ export class PublishPage {
     }
   }
   
-  
+  //时间戳转换时间
+  timestampToTime(timestamp) {
+    var date = new Date(timestamp);
+    // var Y = date.getFullYear() + '年';
+    var M = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1) + '月';
+    var D = date.getDate() + '日 ';
+    var h = date.getHours() + ':';
+    var m = date.getMinutes();
+    // var s = date.getSeconds();
+    return M + D + h + m ;
+  }
 
 }
