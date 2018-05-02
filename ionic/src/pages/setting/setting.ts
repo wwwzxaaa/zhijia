0fd4d78a0a635279540f5751f88970d438de07d7
WuXiaoTon <974124343@qq.com>
2018-05-02 17:24:30 +0800

    setting list

--- ionic/src/pages/setting/setting.ts
+++ ionic/src/pages/setting/setting.ts
@@ -18,35 +18,19 @@ export class SettingPage {
   constructor(public navCtrl: NavController, public navParams: NavParams,private app:App) {
   }
 
-  button1_3=[
-    {src:'assets/imgs/per (1).png',name:'芽币'},
-    {fun:'mypub()',src:'assets/imgs/per (2).png',name:'我发布的'},
-    {src:'assets/imgs/per (3).png',name:'我合作的'}
+  settings=[
+    {src:'assets/imgs/setting_01.png',name:'我的关注'},
+    {/*fun:'mypub()',*/src:'assets/imgs/setting_02.png',name:'消费'},
+    {src:'assets/imgs/setting_03.png',name:'我的社区'},
+    {fun:'mypub()',src:'assets/imgs/setting_04.png',name:'软件设置'}
   ]
-  button4_9=[
-    {src:'assets/imgs/per (4).png',name:'我的收藏'},
-    {src:'assets/imgs/per (5).png',name:'我的草稿箱'},
-    {src:'assets/imgs/per (6).png',name:'视频发布'},
-    {src:'assets/imgs/per (7).png',name:'完善信息'},
-    {src:'assets/imgs/per (8).png',name:'VIP等级'},
-    {src:'assets/imgs/per (9).png',name:'账户安全'},
-    {src:'assets/imgs/per (10).png',name:'帮助与反馈'},
-    {src:'assets/imgs/per (11).png',name:'设置'}
-  ]
-  /*
-  mybtn1(i){
-    if(i==1){
+  mybtn(i){
+    if(i==3){
       this.app.getRootNav().push(MIssuePage);
-    }else if(i==2){
-      this.app.getRootNav().push(MCoopreationPage);
     }
   }
-  mybtn2(i){
-    if(i==7){
-      this.app.getRootNav().push(SettingPage);
-    }
 }
-*/
+
   ionViewDidLoad() {
     console.log('ionViewDidLoad SettingPage');
   }
