9b310d040b870dd2ed1d5b58737c4edd52a52f47
Mxtzz <mazw2016@hotmail.com>
2018-05-15 11:02:57 +0800

    通知界面

--- ionic/src/pages/about/about.ts
+++ ionic/src/pages/about/about.ts
@@ -11,4 +11,11 @@ export class AboutPage {
 
   }
 
+  info = [
+    {
+      icon: 'assets/about/zhuoda.jpg', name: '卓达物业', time: '2018年5月2日',
+      pic: 'assets/about/attention.png', article: '来充钱啊不充钱你怎么变得更强！！！',
+      good: 6
+    }
+  ];
 }
