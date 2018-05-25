f1e7ffbaeacd2ee4378ce1bf8983bf9a3f562dee
WuXiaoTon <974124343@qq.com>
2018-05-25 11:45:57 +0800

    后台获取

--- ionic/src/pages/about/about.ts
+++ ionic/src/pages/about/about.ts
@@ -11,13 +11,32 @@ export class AboutPage {
   constructor(public http:Http,public jsonp:Jsonp,public navCtrl: NavController) {
 
   }
+  content;
   ionViewDidLoad() {
-    console.log('ionViewDidLoad LoginPage');
+    var HTTP=this.http;
+
+    console.log('通知');//页面验证
+
+    //后台信息读取
+    //id、title
+    this.http.get('http://localhost:7000/api/v1/content').subscribe(data=>{
+      this.content=JSON.parse(data['_body']);
+      console.log(this.content);
+      getPic();
+    },err=>{
+      console.log(err);
+    });
+    //图片
+    function getPic(){
+      HTTP.get('http://localhost:7000/api/v1/file').subscribe(data=>{
+        console.log(JSON.parse(data['_body']));
+      })
+    }
   }
   info = [
     {
       icon: 'assets/about/zhuoda.jpg', name: '卓达物业', time: '2018年5月2日',
-      pic: 'assets/about/attention.png', article: '来充钱啊不充钱你怎么变得更强！！！',
+      pic: 'http://localhost:7000/uploaded/files/3_DAYS.jpg', article: '来充钱啊不充钱你怎么变得更强！！！',
       good: 6
     }
   ];
