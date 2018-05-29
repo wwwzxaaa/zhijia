49cbe2b969a543d407874040b706b02761151460
WuXiaoTon <974124343@qq.com>
2018-05-29 14:02:19 +0800

    首页按钮，通知页接口

--- ionic/src/pages/about/about.ts
+++ ionic/src/pages/about/about.ts
@@ -14,22 +14,32 @@ export class AboutPage {
   content;
   ionViewDidLoad() {
     var HTTP=this.http;
-
+    var list_id;
+    var pic_id;
     console.log('通知');//页面验证
 
     //后台信息读取
-    //id、title
+    //_id、title
     this.http.get('http://localhost:7000/api/v1/content').subscribe(data=>{
       this.content=JSON.parse(data['_body']);
-      console.log(this.content);
+      list_id=this.content.data;
+      //list_id: 
+        //.created  创建时间
+        //._id  每个通知的id
+        //.gallery[?]  图片id
       getPic();
     },err=>{
       console.log(err);
     });
     //图片
     function getPic(){
+      console.log(list_id);
       HTTP.get('http://localhost:7000/api/v1/file').subscribe(data=>{
-        console.log(JSON.parse(data['_body']));
+        pic_id=JSON.parse(data['_body']).data;
+        console.log(pic_id);
+        //pic_id: 
+        //._id  图片知的id
+        //.url  图片网址
       })
     }
   }
--- ionic/src/pages/home/home.ts
+++ ionic/src/pages/home/home.ts
@@ -10,5 +10,27 @@ export class HomePage {
   constructor(public navCtrl: NavController) {
 
   }
+  home_button=[
+    {src:'assets/imgs/home_01.png'},
+    {src:'assets/imgs/home_02.png'},
+    {src:'assets/imgs/home_03.png'},
+    {src:'assets/imgs/home_04.png'},
+    {src:'assets/imgs/home_05.png'},
+    {src:'assets/imgs/home_06.png'},
+    {src:'assets/imgs/home_07.png'},
+    {src:'assets/imgs/home_08.png'}
+  ]
 
+  annex(id){
+    console.log(id);
+    if(id==0){}
+    if(id==1){}
+    if(id==2){}
+    if(id==3){}
+    if(id==4){}
+    if(id==5){}
+    if(id==6){}
+    if(id==7){}
+    if(id==8){}
+  }
 }
