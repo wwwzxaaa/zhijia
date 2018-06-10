e31e938c3969f29abdbb0226e50dd6abbc408bef
Mxtzz <mazw2016@hotmail.com>
2018-06-10 23:48:00 +0800

    完善通知页

--- ionic/src/pages/about/about.ts
+++ ionic/src/pages/about/about.ts
@@ -1,97 +1,129 @@
 import { Component } from '@angular/core';
-import { NavController,IonicPage, NavParams,App } from 'ionic-angular';
-import { Http,Jsonp } from '@angular/http';
-import "rxjs/Rx";
+import { IonicPage, NavController, NavParams, App, Events } from 'ionic-angular';
+import { Http } from '@angular/http';
 import { PertainAboutPage } from '../pertain-about/pertain-about';
-declare var $:any; 
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
 export class AboutPage {
-  constructor(public http:Http,public jsonp:Jsonp,public navCtrl: NavController,private app:App) {
+  constructor(
+    public http:Http,
+    public navCtrl: NavController,
+    private app:App
+  ) {
 
   }
-  Info=[];
-  getInfo(info){
-    this.Info=info;
-  }
-  ionViewDidEnter() {
-    var HTTP=this.http,info=[],list_id,pic_id;
-    var that=this;
-    // console.log('通知');//页面验证
-    //后台信息读取
-    //_id、title
-    HTTP.get('http://39.105.139.109:7000/api/v1/content').subscribe(data=>{
-      list_id=JSON.parse(data['_body']).data; //list_id:  .created  创建时间 ._id  每个通知的id .gallery[?]  图片id .title  标题
-      getPic();
+
+  infoAll = []
+  info = []
+  ionViewWillEnter() {
+
+    this.http.get('http://39.105.139.109:7000/api/v1/content/', {}).subscribe(data => {
+      this.infoAll = JSON.parse(data['_body']).data;
+      // author = 5b10f91ba5a3c70ede59023b
+      // console.log(this.infoAll)
+      for(let i=0; i<this.infoAll.length; i++){
+        if(this.infoAll[i].author == '5b10f91ba5a3c70ede59023b'){
+          this.infoAll[i].created =this.timeTotime(this.infoAll[i].created)
+          this.info.unshift(this.infoAll[i])
+          // console.log(this.info)
+          // this.info[i].time = this.timeTotime(this.infoAll[i].created)
+        }
+      }
+
+    }, err => {
+      console.log(err);
     });
-    //图片
-    function getPic(){
-      HTTP.get('http://39.105.139.109:7000/api/v1/file').subscribe(data=>{
-        pic_id=JSON.parse(data['_body']).data;//pic_id: //._id  图片知的id //.url  图片网址
-        changeInfo();
-      })
-    }
-    function changeInfo(){
-      function List(id,time,title,pic_url,gallery){this.id=id;this.time=time;this.title=title;this.pic_url=pic_url;this.gallery=gallery;}
-      //拼接对象
-      for(var i=0;i<list_id.length;i++){
-      //列表
-        var id=list_id[i]._id;
-        var title=list_id[i].title;//`````````````标题:title
-        var time=list_id[i].created.slice(0,10);//`````````````时间:time
-        var gallery=list_id[i].gallery;
-      //图片
-        var pic_url=undefined;//`````````````图片url:pic_url
-        for(var j=0;j<pic_id.length;j++){
-          if(pic_id[j]._id==gallery[0]){
-              pic_url=pic_id[j].url;
-          }
-        }//for2结束
-          info.push(new List(id,time,title,pic_url,gallery));
-      }//for1结束
-      that.getInfo(info);
-    }//chengeInfo(){}
-    
-  }//页面加载
-
-  detail(num){
-    this.app.getRootNav().push(PertainAboutPage,{info:this.Info[num]});
   }
   
- 
-    /*属性列表
-      list_id[]._id 
-      list_id[].created
-      list_id[].gallery
-      pic_id[]._id
-      pic_id[].url
-      content._id
-      content.content
-      content.title
-      */
-     
-      //内容
-          //}
-      /*
-        var the_content;
-        var id_url='http://localhost:7000/api/v1/content/'+list_id[i]._id;
-        //function getContent(){
-          HTTP.get(id_url).subscribe(data=>{
-              content=JSON.parse(data['_body']).data;
-              //content:
-              //.content  通知内容
-              //.title  通知标题
-              //._id  每个通知的id
-              the_content=content.content;
-            });
+  ionViewDidLeave(){
+    this.info = []
+  }
 
-            通知二级页面：用id拿内容content.content，about的Info里添加list_id[].gallery项，起名gallery申请文件接口拿图写入数组。
-            content部分：
-            头像-名称-时间-标题
-            <div>{{内容}}
-            <div *ngfor><img src="{{gallery.src}}">
-            */
+  main(id){
+    this.app.getRootNav().push(PertainAboutPage,{
+      id:this.info[id]._id
+    });
+  }
+
+  //时间戳转换时间
+  timeTotime(time){
+    let y = time.slice(5,10)
+    let h = time.slice(11,16)
+    return y+' '+h
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // Info=[];
+  // getInfo(info){
+  //   this.Info=info;
+  // }
+  // ionViewDidEnter() {
+  //   var HTTP=this.http,info=[],list_id,pic_id;
+  //   var that=this;
+  //   // console.log('通知');//页面验证
+  //   //后台信息读取
+  //   //_id、title
+  //   HTTP.get('http://39.105.139.109:7000/api/v1/content').subscribe(data=>{
+  //     list_id=JSON.parse(data['_body']).data; //list_id:  .created  创建时间 ._id  每个通知的id .gallery[?]  图片id .title  标题
+  //     getPic();
+  //   });
+  //   //图片
+  //   function getPic(){
+  //     HTTP.get('http://39.105.139.109:7000/api/v1/file').subscribe(data=>{
+  //       pic_id=JSON.parse(data['_body']).data;//pic_id: //._id  图片知的id //.url  图片网址
+  //       changeInfo();
+  //     })
+  //   }
+  //   function changeInfo(){
+  //     function List(id,time,title,pic_url,gallery){this.id=id;this.time=time;this.title=title;this.pic_url=pic_url;this.gallery=gallery;}
+  //     //拼接对象
+  //     for(var i=0;i<list_id.length;i++){
+  //     //列表
+  //       var id=list_id[i]._id;
+  //       var title=list_id[i].title;//`````````````标题:title
+  //       var time=list_id[i].created.slice(0,10);//`````````````时间:time
+  //       var gallery=list_id[i].gallery;
+  //     //图片
+  //       var pic_url=undefined;//`````````````图片url:pic_url
+  //       for(var j=0;j<pic_id.length;j++){
+  //         if(pic_id[j]._id==gallery[0]){
+  //             pic_url=pic_id[j].url;
+  //         }
+  //       }//for2结束
+  //         info.push(new List(id,time,title,pic_url,gallery));
+  //     }//for1结束
+  //     that.getInfo(info);
+  //   }//chengeInfo(){}
+    
+  // }//页面加载
+
+  // detail(num){
+  //   this.app.getRootNav().push(PertainAboutPage,{info:this.Info[num]});
+  // }
+  
+ 
+   
   
 }
--- ionic/src/pages/pertain-about/pertain-about.ts
+++ ionic/src/pages/pertain-about/pertain-about.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams} from 'ionic-angular';
-import { App } from 'ionic-angular';
-import { Http,Jsonp } from '@angular/http';
+import { Http } from '@angular/http';
 
 @IonicPage()
 @Component({
@@ -10,54 +9,30 @@ import { Http,Jsonp } from '@angular/http';
 })
 export class PertainAboutPage {
 
-  constructor(public http:Http,public jsonp:Jsonp,public navCtrl: NavController, public navParams: NavParams,private app:App) {
-    this.info=navParams.get('info');
+  constructor(
+    public http:Http,
+    public navCtrl: NavController, 
+    public navParams: NavParams
+  ) {
+    this.id=navParams.get('id');
+    // console.log(this.id)
   }
-
+  id = ''
+  info = {created:'',title:'',content:''}
   ionViewDidLoad() {
-    console.log(this.info);
-        var id_url='http://39.105.139.109:7000/api/v1/content/'+this.info.id;
-          this.http.get(id_url).subscribe(data=>{
-             // content=JSON.parse(data['_body']).data;
-              //content:
-              //.content  通知内容
-              //.title  通知标题
-              //._id  每个通知的id
-              this.content=JSON.parse(data['_body']).data.content;
-            });
-            this.http.get('http://39.105.139.109:7000/api/v1/file').subscribe(data=>{
-              console.log(JSON.parse(data['_body']).data);
-              for(var i=0;i<this.info.gallery.length;i++){
-                for(var j=0;j<JSON.parse(data['_body']).data.length;j++){
-                  if(JSON.parse(data['_body']).data[j]._id==this.info.gallery[i]){
-                    this.url_list.push(JSON.parse(data['_body']).data[j].url)
-                  }
-                }
-              }
-              //JSON.parse(data['_body']).data;//pic_id: //._id  图片知的id //.url  图片网址
-            })
+    this.http.get('http://39.105.139.109:7000/api/v1/content/'+this.id,{}).subscribe(data=>{
+      this.info = JSON.parse(data['_body']).data
+      console.log(this.info)
+      this.info.created =this.timeTotime(this.info.created)
+    },err=>{
+      console.log(err);
+    });
   }
-  info;
-  content;
-  url_list=[];
-      /*
-        var the_content;
-        var id_url='http://39.105.139.109:7000/api/v1/content/'+list_id[i]._id;
-        //function getContent(){
-          HTTP.get(id_url).subscribe(data=>{
-              content=JSON.parse(data['_body']).data;
-              //content:
-              //.content  通知内容
-              //.title  通知标题
-              //._id  每个通知的id
-              the_content=content.content;
-            });
 
-            function getPic(){
-              HTTP.get('http://39.105.139.109:7000/api/v1/file').subscribe(data=>{
-                pic_id=JSON.parse(data['_body']).data;//pic_id: //._id  图片知的id //.url  图片网址
-                changeInfo();
-      })
-    }
-            */
+  //时间戳转换时间
+  timeTotime(time){
+    let y = time.slice(5,10)
+    let h = time.slice(11,16)
+    return y+' '+h
+  }
 }
