ae31dae841dcec471f4b744caf9df35e107ace5a
Mxtzz <mazw2016@hotmail.com>
2018-06-15 09:31:58 +0800

    完善报事报修

--- ionic/src/pages/repair/repair.ts
+++ ionic/src/pages/repair/repair.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
+import { Http } from '@angular/http';
+
 
 /**
  * Generated class for the RepairPage page.
@@ -18,14 +20,15 @@ export class RepairPage {
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams,
-    public alertCtrl:AlertController
+    public alertCtrl:AlertController,
+    public http:Http
   ) {
   }
 
   ionViewDidLoad() {
     
   }
-  confirm(){
+  confirm(name: HTMLInputElement, content: HTMLInputElement){
     const confirm = this.alertCtrl.create({
       title: '确定提交吗?',
       message: '',
@@ -39,6 +42,24 @@ export class RepairPage {
         {
           text: '提交',
           handler: () => {
+            let token = localStorage.getItem('user_token');
+            // this.http.get('http://39.105.139.109:7000/api/v1/message/?token='+token, {
+              
+            // }).subscribe(data => {
+            //   console.log(JSON.parse(data['_body']));
+            // }, err => {
+            //   console.log(err);
+            // });
+            this.http.post('http://39.105.139.109:7000/api/v1/message/', {
+              token:token,
+              content:content.value,
+              name:name.value
+            }).subscribe(data => {
+              console.log(JSON.parse(data['_body']));
+            }, err => {
+              console.log(err);
+            });
+            console.log(name.value,content.value)
             this.navCtrl.pop();
            
           }
