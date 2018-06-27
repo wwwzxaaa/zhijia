c46425c6c92ca073df6d60770efd87584101d6dc
genghaoyang <1098485613@qq.com>
2018-06-27 09:01:55 +0800

    恢复页面

---
+++ ionic/src/pages/set-help/set-help.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { SetHelpPage } from './set-help';
+
+@NgModule({
+  declarations: [
+    SetHelpPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(SetHelpPage),
+  ],
+})
+export class SetHelpPageModule {}
