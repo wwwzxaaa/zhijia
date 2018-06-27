2dcd303bb01ecf368b7d4a068091e2743c48d38a
genghaoyang <1098485613@qq.com>
2018-06-27 09:07:39 +0800

    come

---
+++ ionic/src/pages/welcome/welcome.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { RegPage } from '../reg/reg';
+/**
+ * Generated class for the WelcomePage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-welcome',
+  templateUrl: 'welcome.html',
+})
+export class WelcomePage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    // console.log('ionViewDidLoad WelcomePage');
+  }
+  goReg(){
+    this.navCtrl.setRoot(RegPage);
+  }
+}
